26f065683919649a94c9011c98f3b98321353e5a
CDarioB <carlosdario1@gmail.com>
2026-02-21 22:02:50 -0300

    Estructura principal del proyecto

---
+++ MiProyecto/src/MiClase.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "..\\include\\MiClase.h"
+
+using std::cout;
+using std::endl;
+
+/*
+    #include<string>
+    using std::string;
+    
+    No haria falta agregarlo porque esta incluido en MiClase.h
+
+*/
+
+MiClase::MiClase(string n, int e)
+{
+    nombre = n;
+    edad = e;
+}
+ 
+void MiClase::setNombre(string n)
+{
+    nombre = n;
+}
+
+void MiClase::setEdad(int e)
+{
+    edad = e;
+}
+string MiClase::getNombre() const
+{
+    return nombre;
+}
+
+int MiClase::getEdad() const
+{
+    return edad;
+}
+
+void MiClase::imprimir() const
+{
+    cout << nombre << "tiene " << edad << " años" << endl;
+}
+
+
